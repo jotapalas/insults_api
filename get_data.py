7387c328e225fdd1800e3cf21f017a4b7370342e
Juan Palacios Galván <palaciosgalvanj@gmail.com>
2019-03-05 12:27:50 +0100

    Added order_by id to query

--- get_data.py
+++ get_data.py
@@ -5,7 +5,7 @@ from firebase_admin import firestore
 
 def connect_to_db():
   # Use a service account
-  cred = credentials.Certificate('fbio-config.json')
+  cred = credentials.Certificate('fbio-config.json') #configuration file from Firestore
   if not len(firebase_admin._apps):
     firebase_admin.initialize_app(cred)
   db = firestore.client()
@@ -13,7 +13,7 @@ def connect_to_db():
 
 def get_data(table, fields=[]):
   db = connect_to_db()
-  docs = db.collection(table).get()
+  docs = db.collection(table).order_by('id').get()
   result = []
   for doc in docs:
     result.append(doc.to_dict())
--- main.py
+++ main.py
@@ -36,3 +36,6 @@ class All(Resource):
 api.add_resource(Insult, '/api/insults')
 api.add_resource(Comeback, '/api/comebacks')
 api.add_resource(All, '/api/all')
+
+if __name__ == '__main__':
+  app.run(port=1337, debug=True)
